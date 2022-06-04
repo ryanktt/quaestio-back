640c357835d1574f7c070980cb83d76cd9fe9110
Ryan <ryankayrorv@outlook.com>
2022-06-04 15:02:46 +0000

    feat: add validation and rate limit

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
+import rateLimit from 'express-rate-limit';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import requestIp from 'request-ip';
 
 async function bootstrap(): Promise<void> {
 	const app = await NestFactory.create(AppModule);
-	await app.listen(3000);
+	app.enableCors();
+	app.use(
+		rateLimit({
+			message: 'Too many requests, please try again later.',
+			windowMs: 5 * 60 * 1000,
+			max: 100,
+		}),
+	);
+	app.use(requestIp.mw());
+	await app.listen(4000);
 }
 void bootstrap();

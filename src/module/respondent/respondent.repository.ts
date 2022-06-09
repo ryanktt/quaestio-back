b8564356a90b5156721d38d592bc120b90e0f193
Ryan <ryankayrorv@outlook.com>
2022-06-09 18:08:12 +0000

    feat: add create method on respondant repository

--- src/module/respondent/respondent.repository.ts
+++ src/module/respondent/respondent.repository.ts
@@ -1,5 +1,5 @@
+import { ERespondentErrorCode, ICreateRespondent } from './respondent.interface';
 import { RespondentDocument, RespondentModel } from './respondent.schema';
-import { ERespondentErrorCode } from './respondent.interface';
 
 import { InjectModel } from '@nestjs/mongoose';
 import { Injectable } from '@nestjs/common';
@@ -20,5 +20,16 @@ export class RespondentRepository {
 					originalError: err,
 				});
 			}) as Promise<RespondentDocument | null>;
+	}
+
+	async create(params: ICreateRespondent): Promise<RespondentDocument> {
+		const { userAgent, location, name, email, ip } = params;
+		return this.respondentSchema.create({ userAgent, location, name, email, ip }).catch((err: Error) => {
+			throw new AppError({
+				code: ERespondentErrorCode.CREATE_RESPONDENT_ERROR,
+				message: 'fail to create new respondent',
+				originalError: err,
+			});
+		}) as Promise<RespondentDocument>;
 	}
 }

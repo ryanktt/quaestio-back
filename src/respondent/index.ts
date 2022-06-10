608065cb24cf0031c24b92f232ee8bf6e485ed78
Ryan <ryankayrorv@outlook.com>
2022-06-10 16:26:17 +0000

    feat: add get location by ip respondent helper

--- src/respondent/index.ts
+++ src/respondent/index.ts
@@ -2,3 +2,4 @@ export * from './respondent.repository';
 export * from './respondent.interface';
 export * from './respondent.schema';
 export * from './respondent.module';
+export * from './respondent.helper';

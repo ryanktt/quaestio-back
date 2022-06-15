b5a5ce167d817bbfa83d59d388eabab964d73200
Ryan <ryankayrorv@outlook.com>
2022-06-15 11:18:03 +0000

    feat: add discriminator definitions to quiz mongoose module

--- src/quiz/quiz.module.ts
+++ src/quiz/quiz.module.ts
@@ -1,11 +1,23 @@
-import { QuizSchema } from './quiz.schema';
+import { QuizExamSchema, QuizSchema, QuizSurveySchema } from './quiz.schema';
 
 import { forwardRef, Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from 'src/user';
 
 @Module({
-	imports: [MongooseModule.forFeature([{ name: 'Quiz', schema: QuizSchema }]), forwardRef(() => UserModule)],
+	imports: [
+		MongooseModule.forFeature([
+			{
+				name: 'Quiz',
+				schema: QuizSchema,
+				discriminators: [
+					{ name: 'QuizSurvey', schema: QuizSurveySchema },
+					{ name: 'QuizExam', schema: QuizExamSchema },
+				],
+			},
+		]),
+		forwardRef(() => UserModule),
+	],
 	providers: [],
 	exports: [],
 })

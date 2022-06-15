23a09e19ceb5682084a09d0784f5a62f3d296fab
Ryan <ryankayrorv@outlook.com>
2022-06-15 21:25:45 +0000

    refactor: remove unused code

--- src/questionnaire/questionnaire.repository.ts
+++ src/questionnaire/questionnaire.repository.ts
@@ -1,11 +1,4 @@
-import {
-	QuestionnaireModel,
-	QuestionnaireDocument,
-	QuestionnaireExamModel,
-	QuestionnaireSurveyModel,
-	QuestionnaireExamDocument,
-	QuestionnaireSurveyDocument,
-} from './questionnaire.schema';
+import { QuestionnaireModel, QuestionnaireDocument } from './questionnaire.schema';
 import { EQuestionnaireErrorCode } from './questionnaire.interface';
 
 import { InjectModel } from '@nestjs/mongoose';
@@ -14,37 +7,7 @@ import { AppError } from '@utils/*';
 
 @Injectable()
 export class QuestionnaireRepository {
-	constructor(
-		@InjectModel('QuestionnaireSurvey') private readonly surveySchema: QuestionnaireSurveyModel,
-		@InjectModel('QuestionnaireExam') private readonly examSchema: QuestionnaireExamModel,
-		@InjectModel('Questionnaire') private readonly questionnaireSchema: QuestionnaireModel,
-	) {}
-
-	async fetchExamsByIds(examIds: string[]): Promise<QuestionnaireDocument[]> {
-		return this.examSchema
-			.find({ _id: { $in: examIds } })
-			.exec()
-			.catch((err: Error) => {
-				throw new AppError({
-					code: EQuestionnaireErrorCode.FETCH_QUIZZES_ERROR,
-					message: 'fail to fetch questionnaire exams by ids',
-					originalError: err,
-				});
-			}) as Promise<QuestionnaireExamDocument[]>;
-	}
-
-	async fetchSurveysByIds(surveyIds: string[]): Promise<QuestionnaireSurveyDocument[]> {
-		return this.surveySchema
-			.find({ _id: { $in: surveyIds } })
-			.exec()
-			.catch((err: Error) => {
-				throw new AppError({
-					code: EQuestionnaireErrorCode.FETCH_QUIZZES_ERROR,
-					message: 'fail to fetch questionnaire surveys by ids',
-					originalError: err,
-				});
-			}) as Promise<QuestionnaireSurveyDocument[]>;
-	}
+	constructor(@InjectModel('Questionnaire') private readonly questionnaireSchema: QuestionnaireModel) {}
 
 	async fetchByIds(questionnaireIds: string[]): Promise<QuestionnaireDocument[]> {
 		return this.questionnaireSchema

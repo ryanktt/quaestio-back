6f7824cd3939e497dfa13c5c23dd74b1f6569d77
Ryan <ryankayrorv@outlook.com>
2022-06-17 15:44:02 +0000

    feat: add create questionnaire quiz mutation

--- src/questionnaire/questionnaire.resolver.ts
+++ src/questionnaire/questionnaire.resolver.ts
@@ -1,16 +1,32 @@
-import { Questionnaire } from './questionnaire.schema';
+import { Questionnaire, QuestionnaireQuiz } from './questionnaire.schema';
+import { QuestionDiscriminatorInput } from './questionnaire.input';
+import { QuestionnaireService } from './questionnaire.service';
 
-import { Resolver, ResolveField, Parent, Context } from '@nestjs/graphql';
+import { Resolver, ResolveField, Parent, Context, Mutation, Args } from '@nestjs/graphql';
 import { Admin, AdminDocument } from 'src/user';
+import { IAdminContext } from 'src/session';
 import { ILoaders } from 'src/app.loaders';
+import { Role } from '../utils/utils.auth';
 
 @Resolver(() => Questionnaire)
 export class QuestionnaireResolver {
+	constructor(private readonly questionnaireService: QuestionnaireService) {}
+
 	@ResolveField(() => Admin)
 	async user(
 		@Context('loaders') { userLoader }: ILoaders,
 		@Parent() questionnaire: Questionnaire,
 	): Promise<AdminDocument> {
 		return userLoader.load(questionnaire.user) as Promise<AdminDocument>;
+	}
+
+	@Role('Admin')
+	@Mutation(() => QuestionnaireQuiz)
+	async createQuestionnaireQuiz(
+		@Context('req') { user }: IAdminContext,
+		@Args('questions', { type: () => [QuestionDiscriminatorInput] }) questions: QuestionDiscriminatorInput[],
+		@Args('title') title: string,
+	): Promise<QuestionnaireQuiz> {
+		return this.questionnaireService.createQuestionnaireQuiz({ questions, title, user });
 	}
 }

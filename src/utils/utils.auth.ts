2dbc2df006730d8fe9cb0d6e6c23da1208b6b520
Ryan <ryankayrorv@outlook.com>
2022-06-18 19:58:10 +0000

    feat: enhance validate user doc access utils auth function

--- src/utils/utils.auth.ts
+++ src/utils/utils.auth.ts
@@ -1,9 +1,9 @@
 import { AppError, EGeneralErrorCode } from './utils.error';
 import { UtilsPromise } from './utils.promise';
+import { DocumentType } from './utils.schema';
 
 import { CustomDecorator, Injectable, SetMetadata } from '@nestjs/common';
-import { DocumentType } from '@typegoose/typegoose';
-import { EUserRole, UserDocument } from 'src/user';
+import { EUserRole } from 'src/user';
 
 export const Role = (role: keyof typeof EUserRole): CustomDecorator<string> => SetMetadata('role', role);
 
@@ -11,14 +11,21 @@ export const Role = (role: keyof typeof EUserRole): CustomDecorator<string> => S
 export class UtilsAuth {
 	constructor(private readonly utilsPromise: UtilsPromise) {}
 
-	async validateUserDocsAccess<T>(user: UserDocument, docs: DocumentType<T>[]): Promise<void> {
+	/**Asserts reference relations between documents */
+	async validateUserDocAccess<T, U>(
+		docToVal: DocumentType<U> | undefined,
+		refDocsArr: { doc: DocumentType<T>; refKey: keyof DocumentType<U> }[],
+	): Promise<void> {
 		await this.utilsPromise.promisify(() => {
-			if (docs.some((doc) => doc.id !== user.id)) {
-				throw new AppError({
-					message: 'user does not have access to this document',
-					code: EGeneralErrorCode.ACCESS_DENIED,
-				});
-			}
+			if (!docToVal) return;
+			refDocsArr.forEach(({ refKey, doc }) => {
+				if (docToVal[refKey] !== doc.id) {
+					throw new AppError({
+						message: `user does not have access to this document`,
+						code: EGeneralErrorCode.ACCESS_DENIED,
+					});
+				}
+			});
 		});
 	}
 }

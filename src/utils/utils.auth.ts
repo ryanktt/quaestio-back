93460732828dc5504e86095f988354804608a093
Ryan <ryankayrorv@outlook.com>
2022-06-18 15:15:48 +0000

    feat: promisify validateuserdocsaccess func

--- src/utils/utils.auth.ts
+++ src/utils/utils.auth.ts
@@ -1,18 +1,24 @@
+import { AppError, EGeneralErrorCode } from './utils.error';
+import { UtilsPromise } from './utils.promise';
+
 import { CustomDecorator, Injectable, SetMetadata } from '@nestjs/common';
 import { DocumentType } from '@typegoose/typegoose';
 import { EUserRole, UserDocument } from 'src/user';
-import { AppError, EGeneralErrorCode } from './utils.error';
 
 export const Role = (role: keyof typeof EUserRole): CustomDecorator<string> => SetMetadata('role', role);
 
 @Injectable()
 export class UtilsAuth {
-	validateUserDocsAccess<T>(user: UserDocument, docs: DocumentType<T>[]): void {
-		if (docs.some((doc) => doc.id !== user.id)) {
-			throw new AppError({
-				message: 'user does not have access to this document',
-				code: EGeneralErrorCode.ACCESS_DENIED,
-			});
-		}
+	constructor(private readonly utilsPromise: UtilsPromise) {}
+
+	async validateUserDocsAccess<T>(user: UserDocument, docs: DocumentType<T>[]): Promise<void> {
+		await this.utilsPromise.promisify(() => {
+			if (docs.some((doc) => doc.id !== user.id)) {
+				throw new AppError({
+					message: 'user does not have access to this document',
+					code: EGeneralErrorCode.ACCESS_DENIED,
+				});
+			}
+		});
 	}
 }

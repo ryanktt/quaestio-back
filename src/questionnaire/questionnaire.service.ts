2c74b265af6a0fff23112398fd02dc060ecd804b
Ryan <ryankayrorv@outlook.com>
2022-06-18 19:59:18 +0000

    feat: add fetch questionnaire questionnaire service

--- src/questionnaire/questionnaire.service.ts
+++ src/questionnaire/questionnaire.service.ts
@@ -1,17 +1,38 @@
-import { EQuestionnaireErrorCode, ICreateQuestionnaireQuizParams } from './questionnaire.interface';
+import {
+	EQuestionnaireErrorCode,
+	IFetchQuestionnaireParams,
+	ICreateQuestionnaireQuizParams,
+} from './questionnaire.interface';
+import { Question, Questionnaire, QuestionnaireQuiz } from './questionnaire.schema';
 import { QuestionnaireRepository } from './questionnaire.repository';
-import { Question, QuestionnaireQuiz } from './questionnaire.schema';
 import { QuestionnaireHelper } from './questionnaire.helper';
 
-import { Injectable } from '@nestjs/common';
-import { AppError } from '@utils/*';
+import { forwardRef, Inject, Injectable } from '@nestjs/common';
+import { AppError, UtilsAuth } from '@utils/*';
 
 @Injectable()
 export class QuestionnaireService {
 	constructor(
 		private readonly questionnaireRepository: QuestionnaireRepository,
 		private readonly questionnaireHelper: QuestionnaireHelper,
+		@Inject(forwardRef(() => UtilsAuth)) private readonly utilsAuth: UtilsAuth,
 	) {}
+
+	async fetchQuestionnaire({
+		questionnaireSharedId,
+		questionnaireId,
+		user,
+	}: IFetchQuestionnaireParams): Promise<Questionnaire | undefined> {
+		let questionnaire;
+		if (questionnaireId) {
+			questionnaire = await this.questionnaireRepository.fetchById(questionnaireId);
+		} else if (questionnaireSharedId) {
+			questionnaire = await this.questionnaireRepository.fetchBySharedId(questionnaireSharedId);
+		}
+
+		await this.utilsAuth.validateUserDocAccess(questionnaire, [{ doc: user, refKey: 'user' }]);
+		return questionnaire;
+	}
 
 	async createQuestionnaireQuiz({
 		questions: questionDiscriminatorInputArray,
@@ -20,17 +41,17 @@ export class QuestionnaireService {
 	}: ICreateQuestionnaireQuizParams): Promise<QuestionnaireQuiz> {
 		const errCollector = AppError.collectorInstance();
 
-		this.questionnaireHelper.validateTitle(title).catch((err: AppError) => errCollector.collect(err));
-
 		const questions = questionDiscriminatorInputArray.map((discriminatorInput, index) => {
 			const question = this.questionnaireHelper.getQuestionFromQuestionDiscriminatorInput(discriminatorInput);
 			const errObj = {
-				message: `type ${discriminatorInput.type} was specified, but object was not provided at index[${index}]`,
+				message: `object of specified type ${discriminatorInput.type} was not provided at index[${index}]`,
 				code: EQuestionnaireErrorCode.INVALID_QUESTION,
 			};
 			if (!question) errCollector.collect(new AppError(errObj));
 			return question as Question;
 		});
+
+		this.questionnaireHelper.validateTitle(title).catch((err: AppError) => errCollector.collect(err));
 
 		errCollector.run({
 			code: EQuestionnaireErrorCode.CREATE_QUESTIONNAIRE_INVALID_PARAMS,

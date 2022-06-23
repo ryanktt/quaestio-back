a2f044a11c1cdcaa20a9b4c6215cee556cf627db
Ryan <ryankayrorv@outlook.com>
2022-06-23 18:12:05 -0300

    feat: add questionnaire question joi validation schemas

---
+++ src/questionnaire/schema/questionnaire.validation.ts
@@ -0,0 +1,75 @@
+import { EQuestionType } from '../questionnaire.interface';
+import Joi from 'joi';
+
+export const OptionInputValidator = Joi.object().keys({
+	title: Joi.string().required(),
+	feedbackAfterSubmit: Joi.string(),
+	correct: Joi.boolean(),
+});
+
+export const baseQuestionInputValidatorKeys = {
+	type: Joi.string()
+		.valid(...Object.values(EQuestionType))
+		.required(),
+	title: Joi.string().required(),
+	weight: Joi.number().required(),
+	required: Joi.boolean(),
+	description: Joi.string(),
+	showCorrectAnswer: Joi.boolean(),
+};
+
+export const QuestionInputValidator = Joi.object().keys(baseQuestionInputValidatorKeys);
+
+const QuestionSingleChoiceInputValidator = Joi.object().keys({
+	...baseQuestionInputValidatorKeys,
+	options: Joi.array().items(OptionInputValidator).required(),
+	randomizeOptions: Joi.boolean().required(),
+	wrongAnswerFeedback: Joi.string(),
+	rightAnswerFeedback: Joi.string(),
+});
+
+const QuestionMultipleChoiceInputValidator = Joi.object().keys({
+	...baseQuestionInputValidatorKeys,
+	options: Joi.array().items(OptionInputValidator).required(),
+	randomizeOptions: Joi.boolean().required(),
+	wrongAnswerFeedback: Joi.string(),
+	rightAnswerFeedback: Joi.string(),
+});
+
+const QuestionTrueOrFalseInputValidator = Joi.object().keys({
+	...baseQuestionInputValidatorKeys,
+	options: Joi.array().items(OptionInputValidator).required(),
+	wrongAnswerFeedback: Joi.string(),
+	rightAnswerFeedback: Joi.string(),
+});
+
+const QuestionTextInputValidator = Joi.object().keys({
+	...baseQuestionInputValidatorKeys,
+	feedbackAfterSubmit: Joi.string(),
+});
+
+export const QuestionDiscriminatorInputValidator = Joi.object().keys({
+	type: Joi.string()
+		.valid(...Object.values(EQuestionType))
+		.required(),
+	questionMultipleChoice: QuestionMultipleChoiceInputValidator.when('type', {
+		is: Joi.string().valid(EQuestionType.MULTIPLE_CHOICE),
+		then: Joi.required(),
+		otherwise: Joi.optional(),
+	}),
+	questionSingleChoice: QuestionSingleChoiceInputValidator.when('type', {
+		is: Joi.string().valid(EQuestionType.SINGLE_CHOICE),
+		then: Joi.required(),
+		otherwise: Joi.optional(),
+	}),
+	questionTrueOrFalse: QuestionTrueOrFalseInputValidator.when('type', {
+		is: Joi.string().valid(EQuestionType.TRUE_OR_FALSE),
+		then: Joi.required(),
+		otherwise: Joi.optional(),
+	}),
+	questionText: QuestionTextInputValidator.when('type', {
+		is: Joi.string().valid(EQuestionType.TEXT),
+		then: Joi.required(),
+		otherwise: Joi.optional(),
+	}),
+});
